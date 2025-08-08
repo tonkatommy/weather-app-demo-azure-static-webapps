282577d92c6b2e063030aad4ec2801b7723ab606
Tommy G <tommysdevelopment@gmail.com>
2025-08-08 18:49:11 +1200

    Update app branding and improve UI styling
    Changed app name and heading to 'Tommy's Weather App', updated API key, and added Google Fonts for improved typography. Refined CSS for better layout and font consistency.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const cityInput = document.getElementById("input");
 const dispalySection = document.getElementById("display");
 
-
-const API_KEY = "9f1951828ff9489298c50331252502";
+const API_KEY = "5b5818af46cc45b0a00234523252606";
 
 const handleClick = () => {
   let currentCity = cityInput.value;
@@ -14,8 +13,7 @@ const handleClick = () => {
   )
     .then((res) => res.json())
     .then((data) => {
-        dispalySection.innerHTML =
-      ` <p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
+      dispalySection.innerHTML = ` <p>${data.location.name}'s temperature is ${data.current.temp_c}℃.</p>
         <p>It's ${data.current.condition.text}.</p>
         <img src="${data.current.condition.icon}" alt="icon"/>
       `;
